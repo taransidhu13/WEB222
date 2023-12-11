af029284fc0598c6c1a48276d9fd8905e7faecfc
taransidhu13 <153566949+taransidhu13@users.noreply.github.com>
2023-12-11 18:56:38 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+
+window.onload = function(){
+    hiring.addEventListener('click', function (){
+        if (rateHidden){
+            rateLabel.removeAttribute('hidden');
+            rate.removeAttribute('hidden');
+            rate.setAttribute('required', '');
+            rateHidden = false;
+        }
+    });
+
+    question.addEventListener('click', hideRate);
+    comment.addEventListener('click', hideRate);
+
+    form.addEventListener('submit', function(){
+        let fullName = document.querySelector('#full-name').value.trim();
+        let sentFullName = document.querySelector('#full-name');
+        sentFullName.value = fullName;
+        
+        let organization = document.querySelector('#organization').value.trim();
+        let sentOrganization = document.querySelector('#organization');
+        sentOrganization.value = organization;
+
+        let city = document.querySelector('#city').value.trim();
+        let sentCity = document.querySelector('#city');
+        sentCity.value = city;
+        
+        let message = document.querySelector('#message').value.trim();
+        let sentMessage = document.querySelector('#message');
+        sentMessage.value = message;
+        
+        let email = document.querySelector('#email').value.trim();
+        let sentEmail = document.querySelector('#email');
+        sentEmail.value = email;
+    }); 
+
+};
+
+const question = document.querySelector('#question');
+const hiring = document.querySelector('#hiring');
+const form = document.querySelector('#contact-form');
+
+function hideRate() {
+    if(!rateHidden){
+        rate.removeAttribute('required');
+        rate.setAttribute('hidden', '');
+        rateLabel.setAttribute('hidden', '');
+        rateHidden = true;
+    }
+}
+
+function downloadResume() {
+    var resumeURL = "personal/Tarandeep Kaur_Enrolment Services Assistant_Resume.pdf";
+    var downloadLink = document.createElement("a");
+    downloadLink.href = resumeURL;
+    downloadLink.download = "Tarandeep_Kaur_Resume.pdf";
+
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+}
